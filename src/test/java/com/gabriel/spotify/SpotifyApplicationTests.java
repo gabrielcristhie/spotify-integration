625c6072ab9fb0bbf7af4b2a38ac2de3cc8f2ff3
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2024-05-19 21:08:41 -0300

    start project

---
+++ src/test/java/com/gabriel/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
